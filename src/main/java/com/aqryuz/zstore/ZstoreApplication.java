4b25cb13703e69c0f4115807742ebb8003ec1265
AQRyu <daithanh.hua@gmail.com>
2018-11-29 17:58:29 +0700

    create project

---
+++ src/main/java/com/aqryuz/zstore/ZstoreApplication.java
@@ -0,0 +1,12 @@
+package com.aqryuz.zstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZstoreApplication.class, args);
+	}
+}
